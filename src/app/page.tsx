934d01deb2ea8d3035eede83560938a61e4db092
Iaroslav Karpovets <iaroslavk@ext.inditex.com>
2024-05-21 15:34:30 +0300

    use window.location.reload instead of useRouter and refresh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,12 @@ import React, {useCallback} from 'react'
 
 import CvUpload from '../components/upload/cv-upload.tsx';
 import CandidateList from "../components/candidate-list/candidate-list.tsx";
-import { useRouter } from 'next/navigation';
 import Modal from "../components/candidate-list/modal.tsx";
 
 export default function Page() {
-    const router = useRouter();
     const callback = useCallback(() => {
-        router.refresh(); 
-    }, [router]);
+        window.location.reload(); 
+    }, []);
     return (
         <>
             <CvUpload onSuccessSubmit={callback} />
